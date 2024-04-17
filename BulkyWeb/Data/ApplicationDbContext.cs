80b61e58b078819c0f8ccbbc7420664b6943e2d1
Pietro Petracca <pietro.petracca@issgreppi.it>
2024-04-17 10:58:45 +0200

    add docontext

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //ApplicationDbContext riceverà nel costruttore le opzioni per potersi connettere al database
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        //la property Categories permetterà di creare la tabella Categories in fase di Migration e di poter accedere ai dati in
+        //essa contenuti
+        public DbSet<Category> Categories { get; set; } = null!;
+
+    }
+
+
+}
